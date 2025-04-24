7ecd4f2c2b02fa622570e0677c5169843c8df77f
Dylan <nlan4670@gmail.com>
2025-04-25 01:41:16 +0700

    add login gg

---
+++ app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.login');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        $credentials = $request->only('email', 'password');
+    
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('index')
+                ->withSuccess('Signed in');
+        }
+    
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+    
+
+    /**
+     * Registration page
+     */
+    public function createUser()
+    {
+        return view('crud_user.create');
+    }
+ 
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $check = User::create([
+            'name' => $data['name'],
+           
+            'email' => $data['email'],
+            'password' =>\Illuminate\Support\Facades\Hash::make($data['password'])
+        ]);
+
+        return redirect("login");
+    }
+
+    /**
+     * View user detail page
+     */
+    public function readUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.read', ['messi' => $user]);
+    }
+
+    /**
+     * Delete user by id
+     */
+    public function deleteUser(Request $request) {
+        $user_id = $request->get('id');
+        $user = User::destroy($user_id);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            
+            'email' => 'required|email|unique:users,id,'.$input['id'],
+            'password' => 'required|min:6',
+        ]);
+
+       $user = User::find($input['id']);
+       $user->name = $input['name'];
+       $user->email = $input['email'];
+       $user->password = $input['password'];
+       $user->save();
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * List of users
+     */
+    public function listUser()
+    {
+//       // Lấy tất cả người dùng từ bảng users
+        $users = User::all();
+
+        // Truyền dữ liệu vào view
+        return view('crud_user.list', compact('users'));
+    }
+    /**
+     * Sign out
+     */
+    public function signOut() {
+        \Illuminate\Support\Facades\Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
